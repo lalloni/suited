e1a301e79732e1a4cc9a063a9f19a1c3094ddf66
Pablo Lalloni <plalloni@gmail.com>
2013-11-26 15:31:14 -0300

    Introduce excepción para fields duplicados

--- src/main/scala/suited/common.scala
+++ src/main/scala/suited/common.scala
@@ -1,7 +1,9 @@
 package suited
 
+case class DuplicateFieldException(message: String) extends Exception(message)
+
 private[suited] trait UniqueFieldNames {
   private val dupes = fieldNames.groupBy(identity).mapValues(_.size).filter({ case (n, c) ⇒ c > 1 })
-  if (dupes.nonEmpty) throw new IllegalArgumentException(s"Duplicate field(s) ${dupes.keys.mkString(", ")} in $this")
+  if (dupes.nonEmpty) throw DuplicateFieldException(s"Duplicate field(s) ${dupes.keys.mkString(", ")} in $this")
   def fieldNames: List[String]
 }
