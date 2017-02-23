faa10e437842b7735fee7b8a7a5136e483774ae2
dgarcia <dgarcia@afip.gob.ar>
2017-02-23 11:20:50 -0300

    Modifica expresion regular de validación de Dependencia Sarha
    Por el agotamiento de los códigos alfabeticos en las primeras 6
    posiciones del código sarha, se habilito en las mismas el uso de
    los dígitos 1 a 9.
    
    Issue 8312 - Actualizar validación de código SARHA
    https://redmine7.afip.gob.ar/issues/8312

--- src/main/scala/suited/types.scala
+++ src/main/scala/suited/types.scala
@@ -209,7 +209,7 @@ package types {
     val anInteger: Validator[String] = validate[String]("Integer format")(_.matches("""[+-]?\d+"""))
     val aDecimal: Validator[String] = validate[String]("Decimal format")(_.matches("""[+-]?\d+(\.\d+)?"""))
     val CUIT: Validator[String] = validate[String]("CUIT format")(_.matches("""\d{11}"""))
-    val CES: Validator[String] = validate[String]("CES format")(_.matches("""[0A-Z]{6}[0-9]{4}"""))
+    val CES: Validator[String] = validate[String]("CES format")(_.matches("""[0-9A-Z]{6}[0-9]{4}"""))
     val anIPv4: Validator[String] = validate[String]("IPv4 format")(_.matches("""((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"""))
     val anIPv6: Validator[String] = validate[String]("IPv6 format")(_.matches("""(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"""))
     val anIP: Validator[String] = anIPv4 or anIPv6
